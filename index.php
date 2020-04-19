c28ef3bbcc20a3117a97d2d512ecd6a6296e570b
HigorPimentel <higorpimentel@hotmail.com>
2020-04-18 21:05:42 -0300

    Alteração funções index

--- index.php
+++ index.php
@@ -14,47 +14,9 @@
 		
 	</head>
 
-	<body onload="carrega_var ()">
+	<body>
 	
-			<header>				
-					
-								<?php include("menu.php")?>
-			</header>
-
-
-			<div id = "container">
 		
-				
-						
-						<div id="guia_opc">	
-							
-								<?php include("guia_opcoes.php")?>
-							
-						</div>				
-						
-						
-						<div id="container_conteudo">
-							<h1 id="titulo_inicial">SEJA BEM VINDO HIGOR</h1>
-								<div class="barra_separa"> </div>
-									<figure id = "figura_logo">	
-										<img id="img_logo" src="img/logo_padrao.PNG"> </img>
-											<figcaption> SIS WEB - Desenvolvendo Soluções WEB e MOBILE. </figcaption>
-									</figure>	
-								<div class="barra_separa"> </div>
-								
-								
-
-																
-							
-						</div>		
-											
-						
-						
-			</div>
-
-		<script type="text/javascript" src="js/index.js"></script>	
-		<script type="text/javascript" src="js/jquery-3.4.1.min.js" ></script>
-		<script type="text/javascript" src="js/pg1.js"></script>
 		
 
 	</body>

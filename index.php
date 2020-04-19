7f96f27cf6ff3ebf0824e21b5e9db65c250ee541
HigorPimentel <higorpimentel@hotmail.com>
2020-04-18 21:01:09 -0300

    Alteração funções index

--- index.php
+++ index.php
@@ -3,23 +3,23 @@
 <html>
 
 	<head>
-	
-		
-			<title>Controle Estoque - Login</title>
-			<meta charset="utf-8">
-			
-			<link rel="stylesheet" href="css/reset.css">
-			<link rel="stylesheet" href="css/frmt_index.css">
-			<meta name="viewport" content="width=device-width">
-			
-			
-	</head>
-	
-	
-	
-	<body>
 
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title>Pagina Inicial - SIS Web</title>
 		
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/frmt_pagina_inicial.css">
+	
+		
+	</head>
+
+	<body>
+	
+
+
 	</body>
+
+
 
 </html>
